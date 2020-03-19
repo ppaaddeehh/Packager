5866709ee9f5b6e20c44c39854b1ba380d8af81f
patrick parker <paddyloldev@gmail.com>
2020-03-19 23:28:43 +0000

    Initial Commit

---
+++ packager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  packager
+//
+//  Created by patrick parker on 19/03/2020.
+//  Copyright © 2020 patrick parker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
