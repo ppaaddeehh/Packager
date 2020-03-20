56d482dda8b8f903c4abbf794f19a4bb7771de84
patrick parker <paddyloldev@gmail.com>
2020-03-20 01:08:08 +0000

    :package:

--- packager/main.cpp
+++ packager/main.cpp
@@ -5,11 +5,56 @@
 //  Created by patrick parker on 19/03/2020.
 //  Copyright © 2020 patrick parker. All rights reserved.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "Packager.h"
+void startPackager(){
+    DIR *dpdf;
+    struct dirent *epdf;
+    dpdf = opendir("./debs");
+    bool isOk = true;
+    if (dpdf != NULL){
+        while ((epdf = readdir(dpdf))){
+            if(strcmp(epdf->d_name, ".DS_Store") == 0 || strcmp(epdf->d_name, "..") == 0 || strcmp(epdf->d_name, ".") == 0){
+            } else {
+                if(epdf->d_type == 8){
+                    printf("[Packager]: found package: %s \n", epdf->d_name);
+                } else {
+                    printf("[Packager]: %s is of unknown file type\n", epdf->d_name);
+                    isOk = false;
+                }
+            }
+       }
+    } else {
+        isOk = false;
+    }
+    if(isOk){
+        printf("[Packager]: Getting Packages control file\n");
+        if(system("dpkg-scanpackages -m ./debs > Packages")){
+            //dpkg spits out an error regardless :)
+        } else {
+            printf("[Packager]: Retreived information\n");
+        }
+        printf("[Packager]: Generating Packages\n");
+        if(system("bzip2 Packages")){
+            printf("[Packager]: There was an error zipping Packages\n");
+        } else {
+            printf("[Packager]: Finished\n");
+        }
+        return;
+    } else {
+        printf("[Packager]: There was an error, please ensure your file directories are correct\n");
+    }
+    closedir(dpdf);
+}
 
-#include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("[Packager]: " ANSI_COLOR_BLUE  "Thanks for using Packager<3 \n" ANSI_COLOR_RESET);
+    printf("[Packager]: " ANSI_COLOR_BLUE "Any problems, contact me via twitter @paddycodes\n" ANSI_COLOR_RESET);
+    startPackager();
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,18 @@
+Packager
+===============
+
+## packager is an command line tool used to easily generate your Cydia, Sileo, Zebra & Other packagers repo files
+
+## How to use
+
+build using xcode, copy the file in the 'Build' folder into your repo directory and run the following command, and it will do all the work.
+> ./packager
+
+cannot build? a release will be provided.
+
+Requirements
+---------------
+* macOS 10.14 + (version developed on, will probably work on others);
+* bzip2
+* dpkg
+
